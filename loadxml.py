c7810177d26b9b2e0c245546efd0d26a41eee31f
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2023-07-14 00:37:31 +0900

    test1

--- loadxml.py
+++ loadxml.py
@@ -3,38 +3,81 @@
 # Copywrite (c) 2023 Yoichi Tanibayahshi
 #
 import xmltodict
-import json
+import time
+import datetime
+
+INTERVAL_SEC = 5 # sec
+MAX_AGE = 20
 
 xmlfile = '/tmp/nmap-loop.sh.out'
 
-with open(xmlfile, encoding='utf-8') as fp_xml:
-    data_xml = fp_xml.read()
 
-data_dict = xmltodict.parse(data_xml)
+def load_hostdata(xmlfile:str):
+    hostdata = []
 
-for d in data_dict['nmaprun']['host']:
-    if type(d['address']) != list:
-        continue
+    with open(xmlfile, encoding='utf-8') as fp_xml:
+        data_xml = fp_xml.read()
 
-    ip = ''
-    mac = ''
-    hostname = ''
-    vendor = ''
-    
-    for a in d['address']:
-        addr = a['@addr']
-        addrtype = a['@addrtype']
+    data_dict = xmltodict.parse(data_xml)
 
-        if addrtype == 'ipv4':
-            ip = addr
+    for d in data_dict['nmaprun']['host']:
+        if type(d['address']) != list:
+            continue
 
-        if addrtype == 'mac':
-            mac = addr
+        ip = ''
+        mac = ''
+        hostname = ''
+        vendor = ''
 
-        if '@vendor' in a:
-            vendor = a['@vendor']
+        for a in d['address']:
+            addr = a['@addr']
+            addrtype = a['@addrtype']
 
-    if d['hostnames'] is not None:
-        hostname = d['hostnames']['hostname']['@name']
+            if addrtype == 'ipv4':
+                ip = addr
 
-    print('%-15s %s %s (%s)' % (ip, mac, vendor, hostname))
+            if addrtype == 'mac':
+                mac = addr
+
+            if '@vendor' in a:
+                vendor = a['@vendor']
+
+        if ip == '':
+            continue
+
+        if d['hostnames'] is not None:
+            hostname = d['hostnames']['hostname']['@name']
+
+        hostdata.append((ip, mac, vendor, hostname))
+
+        # print('%-15s %s %s (%s)' % (ip, mac, vendor, hostname))
+
+    return hostdata
+
+
+hostage = {}
+while True:
+    print("----- %s" % (datetime.datetime.now()))
+
+    hostdata = load_hostdata(xmlfile)
+
+    for h in hostdata:
+        hostage[h] = MAX_AGE + 1
+
+    count = 0
+    for h in hostage.keys():
+
+        hostage[h] -= 1
+
+        if hostage[h] <= 0:
+            hostage[h] = 0
+            continue
+
+        print("%02d: %-15s %-18s %s (%s)" % (hostage[h],
+                                        h[0], h[1], h[2], h[3]))
+        count += 1
+
+    print("----- %s,  count = %d" %
+          (datetime.datetime.now(), count))
+
+    time.sleep(INTERVAL_SEC)
--- nmap-loop.sh
+++ nmap-loop.sh
@@ -7,9 +7,11 @@ ADDR=$1
 OUTFILE=/tmp/$0.out
 WORKFILE=$OUTFILE.work
 SUDO=sudo
+#NMAP_OPTS="--scan-delay 500ms"
+NMAP_OPTS=""
 
 while true; do
-    $SUDO nmap -sP -oX $WORKFILE $ADDR > /dev/null 2>&1
+    $SUDO nmap $NMAP_OPTS -sP -oX $WORKFILE $ADDR > /dev/null 2>&1
     $SUDO mv -f $WORKFILE $OUTFILE
     date +'%Y-%m-%d %H:%M:%S'
     sleep 1
